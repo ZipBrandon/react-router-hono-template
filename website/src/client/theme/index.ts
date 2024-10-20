c367f0796c466c9460ccf0c1332749a2207ad8f2
lazuee <lazuee.dev@gmail.com>
2024-10-21 03:17:27 +0800

    Initial Commit

---
+++ website/src/client/theme/index.ts
@@ -0,0 +1,14 @@
+import { ClientLoaderArgs } from ".react-router/types/app/+types.root";
+import { useMatches, useNavigation } from "react-router";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+export const isValidTheme = (theme: any): theme is Theme => theme && Object.values(Theme).includes(theme);
+
+export const useTheme = (): Theme => {
+  const theme = useNavigation().formData?.get("theme") || (useMatches()?.[0]?.data as Awaited<ReturnType<ClientLoaderArgs["serverLoader"]>>)?.theme;
+  return isValidTheme(theme) ? theme : Theme.DARK;
+};
